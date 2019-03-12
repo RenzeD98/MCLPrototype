87467fc51bf707203bf6852b738d58e63338cf28
Michael Bakker <michael_snits@hotmail.com>
2019-03-12 14:32:11 +0100

    centralized header

--- resources/views/controlpanel.blade.php
+++ resources/views/controlpanel.blade.php
@@ -1,20 +1,7 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout')
 
-    <title>Laravel</title>
+@section('content')
 
-    <!-- Scripts -->
-    <script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-    <script src="https://js.pusher.com/4.4/pusher.min.js"></script>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-</head>
-<body>
     <h1>Control panel</h1>
     <div class="individual-buttons">
         <input type="button" value="1" onclick="sendUpdate(1)">
@@ -24,85 +11,84 @@
     </div>
     <hr>
     <div>
-        <label>Iteraties  </label><input class="iteration" type="number" value="1"><br>
-        <label>Interval  </label><input class="interval" type="number" value="0"><br>
+        <label>Iteraties </label><input class="iteration" type="number" value="1"><br>
+        <label>Interval </label><input class="interval" type="number" value="0"><br>
         <input id="startButton" type="button" value="start" onclick="startSession()">
     </div>
     <div>
         <h3>Times</h3>
         <ul class="laptimes"></ul>
     </div>
-<script>
-    const url = '{{ url('/') }}';
+    <script>
+        const url = '{{ url('/') }}';
 
-    // open connection to channel
-    let pusher = new Pusher('298ef029374f89470d24', {
-        cluster: 'eu'
-    });
-
-    //subscribe to a channel
-    let channel = pusher.subscribe('MCL_prototype');
-
-    //listen to event
-    channel.bind('toggleStartButton', function (data) {
-        enableButtons();
-    });
-
-    channel.bind('laptimes', function (data) {
-        let laptime = data.laptime;
-        appendLapTime(laptime);
-    });
-
-    function sendUpdate(id) {
-        $.ajax({
-            type: 'POST',
-            url: url + '/api/push-client-update',
-            data: {id: id},
-            success: function(){
-                console.log('verstuurd');
-            }
+        // open connection to channel
+        let pusher = new Pusher('298ef029374f89470d24', {
+            cluster: 'eu'
         });
-    }
-    
-    function startSession() {
-        console.log('startSession');
 
-        $('.laptimes').empty();
+        //subscribe to a channel
+        let channel = pusher.subscribe('MCL_prototype');
 
-        let iterations = $('.iteration').val();
-        let interval = $('.interval').val();
-
-        disableButtons();
-
-        $.ajax({
-            type: 'POST',
-            url: url + '/api/start-session',
-            data: {
-                iterations: iterations,
-                interval: interval
-            },
-            success: function(){
-                console.log('verstuurd');
-            }
+        //listen to event
+        channel.bind('toggleStartButton', function (data) {
+            enableButtons();
         });
-    }
 
-    function disableButtons() {
-        let buttons = $(':button');
-        buttons.prop('disabled', true);
-    }
+        channel.bind('laptimes', function (data) {
+            let laptime = data.laptime;
+            appendLapTime(laptime);
+        });
 
-    function enableButtons() {
-        let buttons = $(':button');
-        buttons.prop('disabled', false);
-    }
+        function sendUpdate(id) {
+            $.ajax({
+                type: 'POST',
+                url: url + '/api/push-client-update',
+                data: {id: id},
+                success: function () {
+                    console.log('verstuurd');
+                }
+            });
+        }
 
-    function appendLapTime(laptime) {
-        let li = document.createElement('li');
-        li.innerText = laptime;
-        $('.laptimes').append(li);
-    }
+        function startSession() {
+            console.log('startSession');
 
-</script>
-</body>
-</html>
+            $('.laptimes').empty();
+
+            let iterations = $('.iteration').val();
+            let interval = $('.interval').val();
+
+            disableButtons();
+
+            $.ajax({
+                type: 'POST',
+                url: url + '/api/start-session',
+                data: {
+                    iterations: iterations,
+                    interval: interval
+                },
+                success: function () {
+                    console.log('verstuurd');
+                }
+            });
+        }
+
+        function disableButtons() {
+            let buttons = $(':button');
+            buttons.prop('disabled', true);
+        }
+
+        function enableButtons() {
+            let buttons = $(':button');
+            buttons.prop('disabled', false);
+        }
+
+        function appendLapTime(laptime) {
+            let li = document.createElement('li');
+            li.innerText = laptime;
+            $('.laptimes').append(li);
+        }
+
+    </script>
+@endsection
--- resources/views/inputpanel.blade.php
+++ resources/views/inputpanel.blade.php
@@ -1,108 +1,92 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout')
 
-    <title>Laravel</title>
+@section('content')
 
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <div class="settings">
+        <input type="button" value="1" onclick="setChannel(1)">
+        <input type="button" value="2" onclick="setChannel(2)">
+        <input type="button" value="3" onclick="setChannel(3)">
+        <input type="button" value="4" onclick="setChannel(4)">
+    </div>
 
-    <!-- Fonts -->
-    <script src="http://code.jquery.com/jquery-3.3.1.min.js"
-            integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-    <script src="https://js.pusher.com/4.4/pusher.min.js"></script>
-</head>
-<body>
+    <div class="inputButtonContainer" style="display: none">
+        <input disabled id="inputButton" type="button" value="click" onclick="returnInput()">
 
-<div class="settings">
-    <input type="button" value="1" onclick="setChannel(1)">
-    <input type="button" value="2" onclick="setChannel(2)">
-    <input type="button" value="3" onclick="setChannel(3)">
-    <input type="button" value="4" onclick="setChannel(4)">
-</div>
+        <nav class="controls">
+            <a href="#" class="button" onClick="stopwatch.start();">Start</a>
+            <a href="#" class="button" onClick="stopwatch.lap();">Lap</a>
+            <a href="#" class="button" onClick="stopwatch.stop();">Stop</a>
+            <a href="#" class="button" onClick="stopwatch.restart();">Restart</a>
+            <a href="#" class="button" onClick="stopwatch.clear();">Clear Laps</a>
+        </nav>
+        <div class="stopwatch"></div>
+        <ul class="results"></ul>
 
-<div class="inputButtonContainer" style="display: none">
-    <input disabled id="inputButton" type="button" value="click" onclick="returnInput()">
+        <script src="{{ URL::asset('js/timer.js') }}"></script>
+    </div>
 
-    <nav class="controls">
-        <a href="#" class="button" onClick="stopwatch.start();">Start</a>
-        <a href="#" class="button" onClick="stopwatch.lap();">Lap</a>
-        <a href="#" class="button" onClick="stopwatch.stop();">Stop</a>
-        <a href="#" class="button" onClick="stopwatch.restart();">Restart</a>
-        <a href="#" class="button" onClick="stopwatch.clear();">Clear Laps</a>
-    </nav>
-    <div class="stopwatch"></div>
-    <ul class="results"></ul>
+    <script>
 
-    <script src="{{ URL::asset('js/timer.js') }}"></script>
-</div>
+        let jsonData = null;
+        const url = '{{ url('/') }}';
 
-<script>
+        function setChannel(id) {
+            // open connection to channel
+            var pusher = new Pusher('298ef029374f89470d24', {
+                cluster: 'eu'
+            });
 
-    let jsonData = null;
-    const url = '{{ url('/') }}';
+            //subscribe to a channel
+            var channel = pusher.subscribe('MCL_prototype');
 
-    function setChannel(id) {
-        // open connection to channel
-        var pusher = new Pusher('298ef029374f89470d24', {
-            cluster: 'eu'
-        });
+            alert('Channel ' + id + ' selected');
 
-        //subscribe to a channel
-        var channel = pusher.subscribe('MCL_prototype');
+            $('.settings').toggle();
+            $('.inputButtonContainer').toggle();
 
-        alert('Channel ' + id + ' selected');
-
-        $('.settings').toggle();
-        $('.inputButtonContainer').toggle();
-
-        //listen to event
-        channel.bind(id, function (data) {
-            //if channelgroup MCL_prototype is called with the selected channel id, then toggle input
-            jsonData = data.message;
-            toggleInput();
-        });
-    }
-
-    function toggleInput() {
-        //toggle the state of the inputButton
-        let inputButton = $('#inputButton');
-        if (inputButton.prop('disabled')) {
-            inputButton.prop('disabled', false);
-            inputButton.css('background-color', 'green');
-
-            //start timer
-            stopwatch.start();
-        } else {
-            inputButton.prop('disabled', true);
-            inputButton.css('background-color', 'red');
+            //listen to event
+            channel.bind(id, function (data) {
+                //if channelgroup MCL_prototype is called with the selected channel id, then toggle input
+                jsonData = data.message;
+                toggleInput();
+            });
         }
-    }
 
-    function returnInput() {
-        stopwatch.stop();
-        let laptime = stopwatch.lap();
-        stopwatch.reset();
+        function toggleInput() {
+            //toggle the state of the inputButton
+            let inputButton = $('#inputButton');
+            if (inputButton.prop('disabled')) {
+                inputButton.prop('disabled', false);
+                inputButton.css('background-color', 'green');
 
-        jsonData = JSON.parse(jsonData);
-        jsonData['laptime'] = laptime;
-        jsonData = JSON.stringify(jsonData);
-
-        //return when the input button is clicked
-        $.ajax({
-            type: 'POST',
-            url: url + '/api/update-session',
-            data: jsonData,
-            success: function (data) {
-                console.log('click');
+                //start timer
+                stopwatch.start();
+            } else {
+                inputButton.prop('disabled', true);
+                inputButton.css('background-color', 'red');
             }
-        });
-        toggleInput();
-    }
+        }
 
-</script>
-</body>
-</html>
+        function returnInput() {
+            stopwatch.stop();
+            let laptime = stopwatch.lap();
+            stopwatch.reset();
+
+            jsonData = JSON.parse(jsonData);
+            jsonData['laptime'] = laptime;
+            jsonData = JSON.stringify(jsonData);
+
+            //return when the input button is clicked
+            $.ajax({
+                type: 'POST',
+                url: url + '/api/update-session',
+                data: jsonData,
+                success: function (data) {
+                    console.log('click');
+                }
+            });
+            toggleInput();
+        }
+
+    </script>
+@endsection
